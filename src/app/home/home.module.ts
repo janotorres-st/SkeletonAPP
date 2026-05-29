632a5e2c0ed0659f8e75e1f0121ea4a5dd49022a
José Torres González <josa.torres@duocuc.cl>
2026-05-29 00:46:53 -0400

    E2

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,8 +3,11 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
 
 
 @NgModule({
@@ -12,7 +15,11 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +8,22 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
   data: any;
+  @ViewChild('tituloHome', { read: ElementRef }) tituloHome!: ElementRef;
+  @ViewChild('inputNombre', { read: ElementRef }) inputNombre!: ElementRef;
+  @ViewChild('inputApellido', { read: ElementRef }) inputApellido!: ElementRef;
+
+  nombre: string = '';
+  apellido: string = '';
+  educacion: string = '';
+  fechaNacimiento: any = null;
 
   constructor(
     private activeRoute: ActivatedRoute, 
-    private router: Router
+    private router: Router,
+    private alertController: AlertController,
+    private animationCtrl: AnimationController
   ) {
     this.activeRoute.queryParams.subscribe(params => {
       if(this.router.currentNavigation()?.extras.state) {
@@ -23,5 +34,60 @@ export class HomePage {
         this.router.navigate(["/login"]);
       }
     });
+  }
+
+  ngAfterViewInit() {
+    const animacionTitulo = this.animationCtrl.create()
+      .addElement(this.tituloHome.nativeElement)
+      .duration(1200)
+      .iterations(1)
+      .fromTo('opacity', '0', '1')
+      .fromTo('transform', 'scale(0.8)', 'scale(1)');
+
+    animacionTitulo.play();
+  }
+
+  limpiarCampos() {
+    this.nombre = '';
+    this.apellido = '';
+    this.educacion = '';
+    this.fechaNacimiento = null;
+
+    const animacionNombre = this.animationCtrl.create()
+      .addElement(this.inputNombre.nativeElement)
+      .duration(1000)
+      .iterations(1)
+      .keyframes([
+        { offset: 0, transform: 'translateX(0px)' },
+        { offset: 0.5, transform: 'translateX(25px)' },
+        { offset: 1, transform: 'translateX(0px)' }
+      ]);
+
+    const animacionApellido = this.animationCtrl.create()
+      .addElement(this.inputApellido.nativeElement)
+      .duration(1000)
+      .iterations(1)
+      .keyframes([
+        { offset: 0, transform: 'translateX(0px)' },
+        { offset: 0.5, transform: 'translateX(25px)' },
+        { offset: 1, transform: 'translateX(0px)' }
+      ]);
+
+    animacionNombre.play();
+    animacionApellido.play();
+  }
+
+  async mostrarAlerta() {
+    const textoAlerta = this.nombre && this.apellido 
+      ? `Su nombre es ${this.nombre} ${this.apellido}`
+      : 'Por favor, ingresa tu nombre y apellido primero.';
+
+    const alert = await this.alertController.create({
+      header: 'Información',
+      message: textoAlerta,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
   }
 }
